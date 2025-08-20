908d15326bf2364a8faf22ba7c9f4779b7b73056
Keanu Villanueva <keanu.villanueva@devsoft.ph>
2025-08-20 12:15:13 +0800

    add cs solution

---
+++ CS/DxThemeVariablesExample/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DxThemeVariablesExample.Models
+{
+    public class Customer
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Name is too short.")]
+        public string Name { get; set; }
+        public int? Age { get; set; }
+    }
+}
